3a5e9060ee77dad9c65bf17c15a72e63b726d09c
MohammedAziz2001 <113575021+MohammedAziz2001@users.noreply.github.com>
2022-11-19 17:22:01 +0530

    Add files via upload

---
+++ Project Development phase/Sprint 2/job.php
@@ -0,0 +1,112 @@
+<?php
+session_start();
+include('config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Counseling</title>
+		<!-- plugins:css -->
+		<link rel="stylesheet" href="../vendors/feather/feather.css">
+		<link rel="stylesheet" href="../vendors/ti-icons/css/themify-icons.css">
+		<link rel="stylesheet" href="../vendors/css/vendor.bundle.base.css">
+		<!-- endinject -->
+		<!-- Plugin css for this page -->
+		<link rel="stylesheet" href="../vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+		<link rel="stylesheet" href="../vendors/ti-icons/css/themify-icons.css">
+		<link rel="stylesheet" type="text/css" href="../js/select.dataTables.min.css">
+		<!-- End plugin css for this page -->
+		<!-- inject:css -->
+		<link rel="stylesheet" href="../css/vertical-layout-light/style.css">
+		<!-- endinject -->
+		<style type="text/css">
+
+			#chart-container {
+				width: 600px;
+				height: auto;
+			}
+		</style>
+		<script type="text/javascript" src="../js/jquery.min.js"> </script>
+		<script type="text/javascript" src="../js/Chart.min.js"> </script>
+	</head>
+	<body>
+		<div class="container-scroller">
+			<!-- partial:partials/_navbar.html -->
+			<?php
+			include('header.php');
+			?>
+			<!-- partial -->
+			<div class="container-fluid page-body-wrapper">
+
+				<?php
+				include('menu.php');
+				?>
+				<!-- partial -->
+				<div class="main-panel">
+					<div class="content-wrapper">
+
+						<div class="row">
+
+							<div class="col-md-12 grid-margin stretch-card">
+								<div class="card">
+									<div class="card-body">
+										<h4 class="card-title">Tata Consultancy Services</h4>
+										<div class="media">
+											<div class="media-body">
+												<p class="card-text">Infosys Ltd was founded in the year 1981 by N.R. Narayana Murthy. The Indian MNC company is offering Information Technology, Outsourcing Services, and Business Consulting. It is among the top 10 software companies in India. It is headquartered in Bangalore, Karnataka. The company also provides application development and management, independent validation, product engineering and management, infrastructure management, enterprise application management, and support and integration services. With Fundoodata paid plans you can get access to more information about the company like the number of employees, turnover, decision-makers details</p>
+												<p class="card-text">
+													The company was established in the year 1997. It is headquartered in Mumbai, Maharashtra. It is offering digital & automation solutions, IT Service Management, and IT Consulting. Its offices are located in top Indian cities as well as North America, Europe, Middle East, South America, Africa, Middle East, Asia Pacific, etc. The company is among the top 10 software companies in India. With Fundoodata paid plans you can get access to more information about the company like the number of employees, turnover, decision-makers details, and more. Click here to know more.
+												</p>
+
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+
+						</div>
+
+
+
+
+
+					</div>
+					<!-- content-wrapper ends -->
+
+					<!-- partial -->
+				</div>
+				<!-- main-panel ends -->
+			</div>
+			<!-- page-body-wrapper ends -->
+		</div>
+		<!-- container-scroller -->
+
+		<!-- plugins:js -->
+		<script src="../vendors/js/vendor.bundle.base.js"> </script>
+		<!-- endinject -->
+		<!-- Plugin js for this page -->
+		<script src="../vendors/chart.js/Chart.min.js"> </script>
+		<script src="../vendors/datatables.net/jquery.dataTables.js"> </script>
+		<script src="../vendors/datatables.net-bs4/dataTables.bootstrap4.js"> </script>
+		<script src="../js/dataTables.select.min.js"> </script>
+
+		<!-- End plugin js for this page -->
+		<!-- inject:js -->
+		<script src="../js/off-canvas.js"> </script>
+		<script src="../js/hoverable-collapse.js"> </script>
+		<script src="../js/template.js"> </script>
+		<script src="../js/settings.js"> </script>
+		<script src="../js/todolist.js"> </script>
+		<!-- endinject -->
+		<!-- Custom js for this page-->
+		<script src="../js/dashboard.js"> </script>
+
+	</body>
+
+</html>
+
