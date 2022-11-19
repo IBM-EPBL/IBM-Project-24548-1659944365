84e9677668bff07cd3ad376d85a69f3c15364237
MohammedAziz2001 <113575021+MohammedAziz2001@users.noreply.github.com>
2022-11-19 17:25:32 +0530

    Add files via upload

---
+++ Project Development phase/Sprint 3/menu.php
@@ -0,0 +1,41 @@
+<!-- partial:partials/_sidebar.html -->
+
+
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+	<ul class="nav">
+		<li class="nav-item">
+		<a class="nav-link" href="dashboard.php">
+		<i class="icon-grid menu-icon"></i>
+		<span class="menu-title">Dashboard</span>
+		</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="ui-basic">
+		<i class="icon-layout menu-icon"></i>
+		<span class="menu-title">Profiles</span>
+		<i class="menu-arrow"></i>
+		</a>
+		<div class="collapse" id="form-elements">
+			<ul class="nav flex-column sub-menu">
+				<li class="nav-item"><a class="nav-link" href="profiles.php">Profiles</a></li>
+				
+			</ul>
+		</div>
+		</li>
+		
+		
+		<li class="nav-item">
+		<a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+		<i class="icon-head menu-icon"></i>
+		<span class="menu-title">Logout</span>
+		<i class="menu-arrow"></i>
+		</a>
+		<div class="collapse" id="auth">
+		<ul class="nav flex-column sub-menu">
+		<li class="nav-item"> <a class="nav-link" href="logout.php"> Logout </a></li>
+
+		</ul>
+		</div>
+		</li>
+	</ul>
+</nav>
